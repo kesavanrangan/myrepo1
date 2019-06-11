d549c9428c5b1095495606e7a89b68aad963cdcb
kesavanrangan <kesavanrangan@gmail.com>
2019-06-12 00:09:34 +0530

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -5,6 +5,6 @@ num = int(sys.argv[1])
 a,b = 0,1
 
 for i in range(num):
-    print("\n---------{%d}--------\n",%(str(i+1)))
+    print("\n--------- Row: %d--------\n" %(i+1))
     print(a)
     a,b = b,a+b
