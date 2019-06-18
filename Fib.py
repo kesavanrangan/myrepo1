fae6d67ad667ee641f71c98c17b8722d0325d4ae
root <root@kesavanrangan.jn>
2019-06-18 11:52:49 +0000

    12345

--- Fib.py
+++ Fib.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 
 num = int(sys.argv[1])
 
@@ -8,5 +9,8 @@ for i in range(num):
     print("\n--------- Row: %d--------\n" %(i+1))
     print(a)
     a,b = b,a+b
+    x = datetime.datetime.now()
+    print(" \n\n")
+    print(x.strftime("%H:%m:%S %b-%d-%Y"))
 
 print("\n The file now ends here!!!")
