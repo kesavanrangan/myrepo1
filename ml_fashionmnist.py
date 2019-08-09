63f5b9feb774f567060efa4c846876e4ff2a7f22
kesavanrangan <kesavanrangan@gmail.com>
2019-08-09 17:42:56 +0530

    Update ml_fashionmnist.py

--- ml_fashionmnist.py
+++ ml_fashionmnist.py
@@ -14,12 +14,14 @@ label_dict = {0: "T-shirt/top",1: "Trouser",2: "Pullover",3: "Dress",4: "Coat",5
 
 
 def ReadCsv():
+    global train
     train = pd.read_csv('fashion_dataset/train.csv')
     test = pd.read_csv('fashion_dataset/test.csv')
+    global X_train,y_train,X_test,y_test
     X_train = np.array(train.iloc[:, 1:])
     y_train = np.array(train.iloc[:, 0]) # target values of training dataset
-    X_test =  np.array(test.iloc[:10, 1:])
-    y_test = np.array(test.iloc[:10, 0]) # target values of testing dataset
+    X_test =  np.array(test.iloc[:25, 1:])
+    y_test = np.array(test.iloc[:25, 0]) # target values of testing dataset
     train.shape
 
 def plot_train():
@@ -34,32 +36,33 @@ def plot_train():
     plt.show()
 
 def plot_test():
-    n_row = 2
+    n_row = 5
     n_col = 5
     plt.figure(figsize=(10,8))
-    for data in final_list[:10]:
+    for data in final_list[:25]:
         print(data, label_dict[data])
     for i in list(range(n_row*n_col)):
         plt.subplot(n_row, n_col, i+1)
         plt.imshow(X_test[i,:].reshape(28,28), cmap="gray")
-        plt.title(y_test[i], size=6.5)
+        plt.title(final_list[i], size=6.5)
     plt.show()
     
 
 def Training():
     model = KNeighborsClassifier(n_neighbors=5)
     model.fit(X_train,y_train)
+    global final_pred
     final_pred = model.predict(X_test)
-    print("The prediction Completed")
-    print(final_pred)
+    print("The prediction Completed \n")
+    #print(final_pred)
     global final_list
     final_list = list(final_pred)
 
 
 def Accuracy():
-    print(metrics.classification_report(y_test, final))
-    accuracy = metrics.accuracy_score(y_test, final)
-    average_accuracy = np.mean(y_test == final) * 100
+    print(metrics.classification_report(y_test, final_pred))
+    accuracy = metrics.accuracy_score(y_test, final_pred)
+    average_accuracy = np.mean(y_test == final_pred) * 100
     print("The average_accuracy is {0:.1f}%".format(average_accuracy))
 
 
